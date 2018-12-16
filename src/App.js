417b4fb91558f23b6d68d53e09a7d903e5e45b36
DATBOI <alexsimons9999@gmail.com>
2018-12-16 10:50:50 -0600

    Moar mt icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CssBaseline from "@material-ui/core/es/CssBaseline/CssBaseline";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Me me big boy
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <CssBaseline>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a className="App-link"
+                 href="https://reactjs.org"
+                 target="_blank"
+                 rel="noopener noreferrer"
+              >
+                Me me big boy
+              </a>
+            </header>
+          </div>
+        </CssBaseline>
+      </React.Fragment>
     );
   }
 }
