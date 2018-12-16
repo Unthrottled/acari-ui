844d1f0fab4cd81675654164af5b095d6da63038
DATBOI <alexsimons9999@gmail.com>
2018-12-16 11:02:12 -0600

    What was their's is now mine

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Thing from './Thing'
 import CssBaseline from "@material-ui/core/es/CssBaseline/CssBaseline";
 
 class App extends Component {
@@ -8,21 +9,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <CssBaseline>
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo"/>
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a className="App-link"
-                 href="https://reactjs.org"
-                 target="_blank"
-                 rel="noopener noreferrer"
-              >
-                Me me big boy
-              </a>
-            </header>
-          </div>
+        <Thing></Thing>
         </CssBaseline>
       </React.Fragment>
     );
