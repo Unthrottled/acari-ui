ac8431f8ad651afa4f66db021184398738988bb9
DATBOI <alexsimons9999@gmail.com>
2018-12-21 10:26:18 -0600

    Separate file for you

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
@@ -6,12 +6,11 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import Reach from './images/reach.png';
-import Monika from './images/just_monika.png';
 import Footer from "./components/Footer";
 import JumboTron from "./components/JumboTron";
 import ProjectCard from "./components/ProjectCard";
 import AppBar from "@material-ui/core/es/AppBar/AppBar";
+import Projects from './Projects';
 
 const styles = theme => ({
   layout: {
@@ -53,54 +52,6 @@ const styles = theme => ({
   },
 });
 
-const featuredProjects = [
-  {
-    title: 'Featured post',
-    date: 'Nov 12',
-    exerpt:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    reach: Reach,
-    displayImage: Monika,
-    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
-  },
-  {
-    title: 'Post title',
-    date: 'Nov 11',
-    exerpt:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    reach: Reach,
-    displayImage: Monika,
-    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
-  },
-  {
-    title: 'Post title',
-    date: 'Nov 11',
-    exerpt:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    reach: Reach,
-    displayImage: Monika,
-    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
-  },
-  {
-    title: 'Post title',
-    date: 'Nov 11',
-    exerpt:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    reach: Reach,
-    displayImage: Monika,
-    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
-  },
-  {
-    title: 'Post title',
-    date: 'Nov 11',
-    exerpt:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    reach: Reach,
-    displayImage: Monika,
-    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
-  },
-];
-
 const App = props => {
   const {classes} = props;
   return (
@@ -125,7 +76,7 @@ const App = props => {
           <main>
             <JumboTron></JumboTron>
             <Grid container spacing={40} className={classes.cardGrid}>
-              {featuredProjects.map(project => (
+              {Projects.map(project => (
                 <Grid item key={project.title} xs={12} md={6}>
                   <ProjectCard project={project}></ProjectCard>
                 </Grid>
---
+++ src/Projects.js
@@ -0,0 +1,52 @@
+import Reach from "./images/reach.png";
+import Monika from "./images/just_monika.png";
+
+const projects = [
+  {
+    title: 'Featured post',
+    date: 'Nov 12',
+    exerpt:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    reach: Reach,
+    displayImage: Monika,
+    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    exerpt:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    reach: Reach,
+    displayImage: Monika,
+    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    exerpt:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    reach: Reach,
+    displayImage: Monika,
+    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    exerpt:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    reach: Reach,
+    displayImage: Monika,
+    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
+  },
+  {
+    title: 'Post title',
+    date: 'Nov 11',
+    exerpt:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    reach: Reach,
+    displayImage: Monika,
+    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
+  },
+]
+
+export default projects;
