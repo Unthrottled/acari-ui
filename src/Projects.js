fd19d38d875a351f10062c547a29f568aae616d6
DATBOI <alexsimons9999@gmail.com>
2018-12-22 11:53:54 -0600

    Wired in the reach icons for each project

--- src/Projects.js
+++ src/Projects.js
@@ -1,13 +1,32 @@
 import Reach from "./images/reach.png";
+import reach_blue from "./images/reach_blue.png";
+import reach_green from "./images/reach_green.png";
+import reach_purple from "./images/reach_purple.png";
+import reach_red from "./images/reach_red.png";
 import Monika from "./images/just_monika.png";
+import purple from "@material-ui/core/es/colors/purple";
+import blue from "@material-ui/core/es/colors/blue";
+import green from "@material-ui/core/es/colors/green";
+import red from "@material-ui/core/es/colors/red";
 
 const projects = [
+  {
+    title: 'Post title 2',
+    date: 'Nov 11',
+    exerpt:
+      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+    reach: reach_purple,
+    color: purple[500],
+    displayImage: Monika,
+    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
+  },
   {
     title: 'Featured post',
     date: 'Nov 12',
     exerpt:
       'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    reach: Reach,
+    reach: reach_blue,
+    color: blue[500],
     displayImage: Monika,
     descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
   },
@@ -16,16 +35,8 @@ const projects = [
     date: 'Nov 11',
     exerpt:
       'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    reach: Reach,
-    displayImage: Monika,
-    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
-  },
-  {
-    title: 'Post title 2',
-    date: 'Nov 11',
-    exerpt:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    reach: Reach,
+    reach: reach_green,
+    color: green[500],
     displayImage: Monika,
     descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
   },
@@ -34,7 +45,8 @@ const projects = [
     date: 'Nov 11',
     exerpt:
       'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    reach: Reach,
+    reach: reach_purple,
+    color: purple[500],
     displayImage: Monika,
     descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
   },
@@ -43,7 +55,8 @@ const projects = [
     date: 'Nov 11',
     exerpt:
       'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    reach: Reach,
+    reach: reach_red,
+    color: red[500],
     displayImage: Monika,
     descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
   },
