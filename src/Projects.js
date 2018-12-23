6e8b4d5b878a5cda2fd4af9b24adde901c234b56
DATBOI <alexsimons9999@gmail.com>
2018-12-23 08:28:37 -0600

    Got the vertx card in place

--- src/Projects.js
+++ src/Projects.js
@@ -34,7 +34,7 @@ const projects = [
     color: blue[500],
     displayImage: hystrix,
     link: 'https://hystrix.acari.io',
-    linkText: 'Visit the site',
+    linkText: 'Visit the Site',
     descriptions: [
       'Much like the actual devices embedded into residential, commercial, and industrial structures.The latter is designed to prevent a massive surge of electricity from flowing through your circuits. While the former prevents your application from trying to execute commands that are going to fail or take a really long time to respond. It is better to fail fast respond with something, than to never respond at all!',
       "Both are employed to protect the user and in the end that is the part that counts.",
@@ -43,15 +43,22 @@ const projects = [
     ],
   },
   {
-    title: 'Post title 1',
+    title: 'Reactive Applications',
     exerpt:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
+      'Who does not like doing more with less? Especially when you have to pay for AWS EC2 usage hours!',
     reach: reach_green,
     color: green[500],
     displayImage: vertx,
-    link: 'https://acari.io',
-    linkText: 'Visit the site',
-    descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
+    link: 'https://wiki.acari.io',
+    linkText: 'Visit the Site',
+    descriptions: [
+      'This project was about utilizing the asyncronous event driven framework Vertx. It started life as the introduction project on the Vertx documentation page, but then evolved into a little bit more!',
+      'The big plus about the asynchronous, event driven (reactive) paradigm, is that you need minimal threads to do any work. Just one thread can chug through the event loop and accomplish so much, without blocking. This works great in tandem with reactive supporting databases such as MongoDB. Meaning that more of the CPU is being utilized, and less threads an working memory has to be used.',
+      'Which means that I can get away with a Micro EC2 instance!',
+
+
+
+  ],
   },
   {
     title: 'Post title 3',
@@ -61,7 +68,7 @@ const projects = [
     color: purple[500],
     displayImage: pen,
     link: 'https://acari.io',
-    linkText: 'Visit the site',
+    linkText: 'Visit the Site',
     descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
   },
   {
@@ -72,7 +79,7 @@ const projects = [
     color: red[500],
     displayImage: A,
     link: 'https://acari.io',
-    linkText: 'Visit the site',
+    linkText: 'Visit the Site',
     descriptions: ["aoeuaoeu", "aoeuaoeuaoeuauoeuaoetsuhsaoetu hsato ehsaot e", "aosetuh lacoebu satoeuhs aoeunsstah satoenshu "],
   },
 ]
